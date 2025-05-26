19bc4601d57a09a9a1b7218d95fb8c31938b6c88
David Ponte <dponte@f0r3s10.codam.nl>
2025-05-26 17:47:06 +0200

    Refactor ft_memchr function for clarity and remove commented-out code

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,40 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	ch;
-	unsigned char	*str;
+	unsigned char	*ptr;
+	unsigned char	byte;
 
-	i = 0;
-	ch = (unsigned char) c;
-	str = (unsigned char *)s;
-	while (i < n)
+	ptr = (unsigned char *)s;
+	byte = (unsigned char)c;
+	while (n--)
 	{
-		if (str[i] == ch)
-			return (&str[i]);
-		i++;
+		if (*ptr == byte)
+			return (ptr);
+		ptr++;
 	}
 	return (NULL);
 }
-
-//#include <stdio.h>
-//#include <string.h>
-//
-//int main ()
-//{
-//	char str[] = "  Hello  ";
-//	void *result_ft = ft_memchr((const void *) str, 'l', 9);
-//    void *result_std = memchr((const void *) str, 'l', 9);
-//
-//	char *src = "/|\x12\xff\x09\0\x42\042\0\42|\\";
-// 	int size = 12;
-//
-//	printf("%c\n", *(char *)result_ft);
-//	printf("%c\n", *(char *)result_std);
-//	printf("%p\n", memchr((const void *) str, 'l', 9));
-//	printf("%p\n", ft_memchr((const void *) str, 'l', 9));
-//	printf("%c\n", *(char *)memchr((const void *) str, 'l', 9));
-//	printf("%c\n", *(char *)ft_memchr((const void *) str, 'l', 9));
-//	return 0;
-//}
-//
